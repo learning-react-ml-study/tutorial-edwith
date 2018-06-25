7b9dfe84a425f0861ed4567d30987531b4749d5e
yang-yeongman <yang-yeongman@a1platform.co.kr>
2018-06-25 23:07:59 +0900

    aa

--- week_5/youngman/scope.js
+++ week_5/youngman/scope.js
@@ -1,7 +1,37 @@
 // var vscope = 'global';
 // function fscope(){
-//     console.log(vscope);
+//     var test = 'local'
+//     console.log(test);
 // }
+// fscope()
+// console.log(test)
+// var my = '홍길동';
+a()
+
+// var a = function(){
+// console.log('1111')
+// }
+
+function a(){
+console.log('1111')
+}
+// (function(){
+//     console.log(my);
+//     var my = '김길동';
+//     (function(){
+//         console.log(my)
+//         var my ='이길동';
+//         console.log(my)
+//     })();
+//     console.log(my);
+// })();
+
+// function a(){
+//     console.log(my)
+//     var my ='이길동';
+//     console.log(my)
+// }
+// a()
 
 // function fscope(){
 //     var vscope = 'local';
@@ -11,16 +41,17 @@
 // fscope();
 // console.log(lv)
 
-//함수밖 선언 
+// 함수밖 선언 
 // var vscope = 'global';
 // function fscope(){
 //     var vscope = 'local';
-//     console.log(vscope);
+//     // console.log(vscope);
 // }
 // fscope();
 // console.log(vscope)
 
-// //함수안 선언 
+
+//함수안 선언 
 // var vscope = 'global';
 // function fscope(){
 //     vscope = 'local';
@@ -31,14 +62,15 @@
 
 //무한루프
 // function a(){
-//     var i = 0;
+//     i = 0;
 // }
+
 // for(var i=0; i < 5; i++){
 //     a()
 //     console.log(i)
 // }
 
-//익명함수
+// 익명함수
 // (function(){
 //     var MYAPP = {}
 //     MYAPP.calculator = {
@@ -57,22 +89,23 @@
 //     // console.log(sum());
 // }())
 
-//java에서는 허용되지 않는 코드 javascript는 fucntion()에 선언하는 변수만 지역변수
+// java에서는 허용되지 않는 코드 javascript는 fucntion()에 선언하는 변수만 지역변수
 // for(var i = 0; i < 1; i++){
 //     var name = 'coding everybody';
 // }
 // console.log(name);
 
+
 //정적 유효범위 
-var i = 5;
+// var i = 5;
  
-function a(){
-    var i = 10;
-    b();
-}
+// function a(){
+//     var i = 10;
+//     b();
+// }
  
-function b(){
-    console.log(i);
-}
- 
-a();
+// function b(){
+//     console.log(i);
+// }
+
+// a();
